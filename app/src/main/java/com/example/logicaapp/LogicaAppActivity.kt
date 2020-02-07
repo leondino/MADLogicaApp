16ce1197dd1ca2421ef22043474e464512133017
Léon Imanuel <leondino0309@gmail.com>
2020-02-07 14:12:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/logicaapp/LogicaAppActivity.kt
@@ -0,0 +1,12 @@
+package com.example.logicaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogicaAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logica_app)
+    }
+}

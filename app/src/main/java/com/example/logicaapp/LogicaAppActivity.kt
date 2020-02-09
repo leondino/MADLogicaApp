5e55328ee232b762ae3f2b60c6be6e05ee8721c2
Léon Imanuel <leondino0309@gmail.com>
2020-02-09 18:49:38 +0100

    Added working code

--- app/src/main/java/com/example/logicaapp/LogicaAppActivity.kt
+++ app/src/main/java/com/example/logicaapp/LogicaAppActivity.kt
@@ -2,11 +2,32 @@ package com.example.logicaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_logica_app.*
 
 class LogicaAppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logica_app)
+        btnSubmit.setOnClickListener { onSubmit() }
+    }
+
+    var correctAnswers = 0
+        get() {
+            var correctAnswers = 0
+            if (answerOne.text.toString() == getString(R.string.text_true))
+                correctAnswers++
+            if (answerTwo.text.toString() == getString(R.string.text_false))
+                correctAnswers++
+            if (answerThree.text.toString() == getString(R.string.text_false))
+                correctAnswers++
+            if (answerFour.text.toString() == getString(R.string.text_false))
+                correctAnswers++
+            return correctAnswers
+        }
+
+    private fun onSubmit(){
+        Toast.makeText(this, getString(R.string.correct_answer_toast, correctAnswers), Toast.LENGTH_LONG).show()
     }
 }
